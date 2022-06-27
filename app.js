544c150f87e940d559a67949e03604ee8dc247e5
Sai Sarath <saisarath2401@gmail.com>
2022-06-27 19:29:52 +0530

    pushing the code to aws and also the changes in the database

--- app.js
+++ app.js
@@ -2,74 +2,76 @@
  * Module dependencies.
  */
 
-var express = require('express');
-var routes = require('./routes');
+var express = require("express");
+var routes = require("./routes");
 
-var user = require('./routes/user');
-var projects = require('./routes/projects');
+var user = require("./routes/user");
+var projects = require("./routes/projects");
 
-var http = require('http');
-var path = require('path');
-var flash = require('connect-flash');
-var ejs = require('ejs');
+var http = require("http");
+var path = require("path");
+var flash = require("connect-flash");
+var ejs = require("ejs");
 
-var dbConnect = require('./config/database');
-dbConnect(process.env.MONGOLAB_URI || 'mongodb://mxgldz:mxgldz@ds151941.mlab.com:51941/d3mindmap');
+var dbConnect = require("./config/database");
+dbConnect();
 // dbConnect(process.env.MONGOLAB_URI || 'mongodb://localhost:27017/MindMap');
 
-var passport = require('./config/passport');
+var passport = require("./config/passport");
 
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.engine('.html', ejs.renderFile);
-app.set('view engine','ejs');
+app.set("port", process.env.PORT || 3000);
+app.set("views", path.join(__dirname, "views"));
+app.engine(".html", ejs.renderFile);
+app.set("view engine", "ejs");
 app.use(express.favicon());
-app.use(express.logger('dev'));
+app.use(express.logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
-app.use(express.cookieParser('your secret here'));
+app.use(express.cookieParser("your secret here"));
 app.use(express.session());
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(function(req,res,next){
-    if (req.isAuthenticated() || req.url == '/login' || req.url == '/signup' ){
-        return next();
-    } else {
-        res.redirect('/login');
-    }
+app.use(express.static(path.join(__dirname, "public")));
+app.use(function (req, res, next) {
+  if (req.isAuthenticated() || req.url == "/login" || req.url == "/signup") {
+    return next();
+  } else {
+    res.redirect("/login");
+  }
 });
 app.use(app.router);
 
-if ('development' == app.get('env')) {
-    app.use(express.errorHandler());
+if ("development" == app.get("env")) {
+  app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
+app.get("/", routes.index);
 
-app.get('/login', user.login);
-app.get('/signup', user.signup);
-app.post('/signup', user.save);
-app.post('/login',
-    passport.authenticate('local', { successRedirect: '/projects/new',
-        failureRedirect: '/login',
-        failureFlash: true })
+app.get("/login", user.login);
+app.get("/signup", user.signup);
+app.post("/signup", user.save);
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/projects/new",
+    failureRedirect: "/login",
+    failureFlash: true,
+  })
 );
-app.get('/logout', user.logout);
+app.get("/logout", user.logout);
 
-app.get('/projects', projects.list);
-app.get('/projects/new', projects.newProject);
-app.get('/projects/:id', projects.findById);
-app.post('/projects', projects.save);
-app.put('/projects/:id', projects.update);
-app.delete('/projects/:id', projects.deleteById);
+app.get("/projects", projects.list);
+app.get("/projects/new", projects.newProject);
+app.get("/projects/:id", projects.findById);
+app.post("/projects", projects.save);
+app.put("/projects/:id", projects.update);
+app.delete("/projects/:id", projects.deleteById);
 
-
-http.createServer(app).listen(app.get('port'), function(){
-    console.log('Express server listening on port ' + app.get('port'));
+http.createServer(app).listen(app.get("port"), function () {
+  console.log("Express server listening on port " + app.get("port"));
 });
